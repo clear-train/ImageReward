03aef19d8cecc6e8362e0c2df145bc2e7f014443
clear-train <111740117+clear-train@users.noreply.github.com>
2023-07-20 12:05:24 +0800

    Update refl_lora.py

--- refl_lora.py
+++ refl_lora.py
@@ -1,6 +1,6 @@
-from ImageReward import ReFL
+from ImageReward import ReFl_lora
 
 if __name__ == "__main__":
-    args = ReFL.parse_args()
-    trainer = ReFL.Trainer("CompVis/stable-diffusion-v1-4", "data/refl_data.json", args=args)
+    args = ReFl_lora.parse_args()
+    trainer = ReFl_lora.Trainer("CompVis/stable-diffusion-v1-4", "data/refl_data.json", args=args)
     trainer.train(args=args)
